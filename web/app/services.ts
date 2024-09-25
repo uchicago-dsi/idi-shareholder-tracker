92fd721ddd2314afde141135ffa92343cf8d3d86
Launa Greer <greerlauna@gmail.com>
2024-09-25 07:23:32 -0500

    Adds missing base URL for API routes

--- web/app/services.ts
+++ web/app/services.ts
@@ -4,7 +4,7 @@ import { InvestmentSearchRequest } from "@/types";
 
 export const investmentService = {
   search: async (request: InvestmentSearchRequest) => {
-    const url = `/api/investments`;
+    const url = `${process.env.NEXT_PUBLIC_DASHBOARD_BASE_URL}/api/investments`;
     const errMsg = `Failed to search investments.`;
 
     return await post(url, request, errMsg);
